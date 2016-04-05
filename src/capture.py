f4afe52f20f76bf867ba365dc3f94b64545b8463
Ivan Arcuschin <iarcuschin@gmail.com>
2016-04-04 21:27:05 -0300

    Muestro lista de capas de cada paquete

--- src/capture.py
+++ src/capture.py
@@ -2,8 +2,26 @@
 import sys
 from scapy.all import *
 
+def getLayersList(pkt):
+    res = []
+    res.append(pkt.name)
+    while pkt.payload:
+        pkt = pkt.payload
+        res.append(pkt.name)
+    return res
+
 def packteHandler(pkt):
     print pkt.summary()
+    print getLayersList(pkt)
+
+count = 0
+def stopper(pkt):
+    global count # Needed to modify global copy of count
+    if count > 50:
+        return True
+    else:
+        count = count + 1
+        return False
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -11,4 +29,4 @@ if __name__ == "__main__":
         print "Usage: " + sys.argv[0] + " <interface>"
     elif len(sys.argv) > 1:
         interface = sys.argv[1]
-        p = sniff(iface = interface, prn = packteHandler)
+        p = sniff(iface = interface, prn = packteHandler, stop_filter = stopper)
