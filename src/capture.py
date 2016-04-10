0f916a4cb032d1b1d3ceed88a9883123151f2495
Ivan Arcuschin <iarcuschin@gmail.com>
2016-04-10 20:00:32 -0300

    Calculo entropia de fuente usando el atributo type de los paquetes

--- src/capture.py
+++ src/capture.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import sys
+import time
 from scapy.all import *
 
 def getLayersList(pkt):
@@ -11,16 +12,27 @@ def getLayersList(pkt):
     return res
 
 types = []
-def packteHandler(pkt):
+def entropiaEthernet(pkt):
     global types
-    print pkt.summary()
-    try:
+    if Ether in pkt:
+        print pkt.summary()
         types.append(pkt.type)
-    except AttributeError:
-        print "No type"
+
+time_start = 0
+def timeStopper(pkt):
+    global time_start, types
+    time_stop = time.time()
+    diff = time_stop - time_start
+    if diff > 2: # 10 segundos
+        if exp == "entropia-tipos":
+            # computar entropia de types
+            calcularEntropia(types)
+        return True
+    else:
+        return False
 
 count = 0
-def stopper(pkt):
+def countStopper(pkt):
     global count # Needed to modify global copy of count
     if count > 50:
         return True
@@ -28,10 +40,33 @@ def stopper(pkt):
         count = count + 1
         return False
 
+def calcularEntropia(lista):
+    elementosDistintos = {}
+    # contamos las apariciones de cada elemento distinto
+    for elem in lista:
+        if elem not in elementosDistintos:
+            elementosDistintos[elem] = 1
+        else:
+            elementosDistintos[elem] = elementosDistintos[elem] + 1
+    # calculamos la entropia
+    entropia = 0
+    print ""
+    print elementosDistintos
+    for elem, apariciones in elementosDistintos.iteritems():
+        proba = float(apariciones)/float(len(lista))
+        print "Simbolo {0} tiene probabilidad {1}".format(elem, proba)
+        entropia += proba * (- math.log(proba))
+
+    print "La entropia de la fuente es {0}".format(entropia)
+
+exp = ""
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print ''
-        print "Usage: " + sys.argv[0] + " <interface>"
-    elif len(sys.argv) > 1:
+        print "Usage: " + sys.argv[0] + " <interface> <exp>"
+        print "\tDonde <exp> puede ser: \"entropia-tipos\", \"nodos-ARP\""
+    elif len(sys.argv) > 2 and sys.argv[2] == "entropia-tipos":
         interface = sys.argv[1]
-        p = sniff(iface = interface, prn = packteHandler, stop_filter = stopper)
+        exp = sys.argv[2]
+        time_start = time.time()
+        p = sniff(iface = interface, prn = entropiaEthernet, stop_filter = timeStopper)
